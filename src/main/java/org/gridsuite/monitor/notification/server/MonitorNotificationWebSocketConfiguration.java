a89c181d6b6264b293b624f837bce7fdd264bb9c
Ghiles ABDELLAH <ghiles.abdellah@rte-france.com>
2026-06-03 14:29:02 +0200

    minor fix

--- src/main/java/org/gridsuite/monitor/notification/server/MonitorNotificationWebSocketConfiguration.java
+++ src/main/java/org/gridsuite/monitor/notification/server/MonitorNotificationWebSocketConfiguration.java
@@ -12,6 +12,7 @@ import org.springframework.web.reactive.HandlerMapping;
 import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
 import org.springframework.web.reactive.socket.WebSocketHandler;
 import org.springframework.web.reactive.socket.server.support.WebSocketHandlerAdapter;
+
 import java.util.HashMap;
 import java.util.Map;
 
